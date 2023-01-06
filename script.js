58a372dbc04457f2ebda569c29e095c20987eee8
Amul <shindeamul76@gmail.com>
2023-01-06 19:07:20 +0530

    created bmi calculator

---
+++ script.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+    const calculate = document.querySelector('#calculate')
+    calculate.addEventListener('click', calculateMass)
+}
+
+const calculateMass = () => {
+
+    const age = document.querySelector('#age').value;
+    const height = document.querySelector('#height').value;
+    const weight = document.querySelector('#weight').value;
+    const result = document.querySelector('#result')
+
+    if (age === '' || height === '' || weight === '') {
+        alert('Please Enter a value')
+    }
+
+    if (age > 120 || age < 2 ) {
+        alert('please provide age value less 120 and equal to 2')
+    }
+
+    
+
+
+    let total = (weight + 9.81)/ (0.01*height) * (0.01 * height);
+
+    result.innerHTML = total
+
+   
+    
+}
+
